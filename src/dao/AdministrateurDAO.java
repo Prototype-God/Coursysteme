356424634905175e8b5acc85e58d2caf98316007
Prototype_God <76643845+Prototype-God@users.noreply.github.com>
2025-04-22 12:03:51 +0200

    Update AdministrateurDAO.java

--- src/dao/AdministrateurDAO.java
+++ src/dao/AdministrateurDAO.java
@@ -1,42 +1,70 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import model.Admin;
+import java.sql.*;
 
-import model.Administrateur;
+/**
+ * Classe permettant l'acces aux donnees de la table administrateur de la BDD
+ * @author groupe7_4
+ * @version 1.0
+ */
 
-public class AdministrateurDAO {
 
-    private Connection connection;
 
-    public AdministrateurDAO(Connection connection) {
-        this.connection = connection;
+public class AdministrateurDAO extends ConnectionDAO {
+	
+	/**
+	 * Construteur 
+	 */
+	private Connection con;
+	
+    public AdministrateurDAO(Connection con) {
+    	super();
+    	this.con=con;
+    	
     }
 
     /**
      * Vérifie si un administrateur avec ce login existe
      */
-    public Administrateur getByCredentials(String username, String password) {
+    public Admin getByCredentials(String username, String password) {
+    	   
+    	   Connection connection=null;
+    	   PreparedStatement stmt = null;
+    	   ResultSet rs = null;
+    	   Admin returnValue = null;
+    	   
         try {
             // Debug info
             System.out.println("Tentative de connexion admin : " + username + " / " + password);
-
+            connection =  DriverManager.getConnection(URL, LOGIN, PASS);
             String sql = "SELECT * FROM ADMINISTRATEUR WHERE UPPER(USERNAME) = ? AND PASSWORD = ?";
-            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt = connection.prepareStatement(sql);
             stmt.setString(1, username.toUpperCase()); // Ignorer la casse pour le nom d'utilisateur
             stmt.setString(2, password);
-
-            ResultSet rs = stmt.executeQuery();
+            //Execution de la requête
+             rs = stmt.executeQuery();
             if (rs.next()) {
                 // Login réussi
-                return new Administrateur(rs.getString("USERNAME"), rs.getString("PASSWORD"));
+            	returnValue = new Admin(rs.getString("USERNAME"), rs.getString("PASSWORD"));
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }finally {
+        	//fermeture du ResultSet,  du PreparedStatement et de la Connexion
+        	try {
+        		if (rs != null) 
+					rs.close();
+        	}catch (Exception ignore) {}
+        	try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (Exception ignore) {
+			}
         }
 
-        return null; // Aucun administrateur trouvé
+        return returnValue; // Aucun administrateur trouvé
     }
+
 }

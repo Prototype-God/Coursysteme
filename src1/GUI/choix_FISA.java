7eb669dca9a9fa90fb722dbccdbe0fc5349aa8cf
Prototype_God <76643845+Prototype-God@users.noreply.github.com>
2025-05-13 12:05:22 +0200

    Update choix_FISA.java

--- src1/GUI/choix_FISA.java
+++ src1/GUI/choix_FISA.java
@@ -1,20 +1,17 @@
 package GUI;
 
-import dao.ConnectionDAO;
-import dao.EtudiantDAO;
-import model.Etudiants;
-import stockage.StudentStatut;
 
+import dao.EtudiantDAO;
+import stockage.StudentStatut;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.awt.Font;
 import java.awt.Color;
 import java.util.List;
 import java.util.ArrayList;
 import dao.ChoixDAO;
+import javax.swing.GroupLayout.Alignment;
 
 public class choix_FISA {
 
@@ -36,66 +33,120 @@ public class choix_FISA {
 	         jframe = new JFrame("Menu FISA");
 	         jframe.getContentPane().setBackground(new Color(223, 209, 228));
 	         jframe.setSize(500, 300);
-	         jframe.getContentPane().setLayout(null);
 	        
 	         //tableau contenant le liste des dominantes
-	         String[] tab_domin = {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"};
+	         //String[] tab_domin = {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"};
+	         
+	         List<String> dominantes = EtudiantDAO.getAllDominantes();
+	         String[] tab_domin = dominantes.toArray(new String[0]);
 	         
 	         JComboBox <String> comboBox = new JComboBox <> (tab_domin);
-	        // comboBox.setModel(new DefaultComboBoxModel(new String[] {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"}));
-	         comboBox.setBounds(93, 61, 258, 36);
-	         jframe.getContentPane().add(comboBox);
 	         
 	         JComboBox <String> comboBox_1 = new JComboBox <> (tab_domin);
-	        // comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"}));
-	         comboBox_1.setBounds(93, 147, 258, 36);
-	         jframe.getContentPane().add(comboBox_1);
 	         
 	         JLabel lblNewLabel = new JLabel("Choix 1");
 	         lblNewLabel.setForeground(new Color(255, 0, 0));
 	         lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 12));
-	         lblNewLabel.setBounds(38, 73, 45, 13);
-	         jframe.getContentPane().add(lblNewLabel);
 	         
 	         JLabel lblChoix = new JLabel("Choix 2");
 	         lblChoix.setForeground(new Color(255, 0, 0));
 	         lblChoix.setFont(new Font("Times New Roman", Font.BOLD, 12));
-	         lblChoix.setBounds(38, 159, 45, 13);
-	         jframe.getContentPane().add(lblChoix);
 	         
 	         JLabel lblNewLabel_1 = new JLabel("      Faites vos choix selon vos préférences");
 	         lblNewLabel_1.setForeground(new Color(128, 0, 128));
 	         lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
-	         lblNewLabel_1.setBounds(53, 10, 345, 25);
-	         jframe.getContentPane().add(lblNewLabel_1);
 	         
 	         JButton btnNewButton = new JButton("VALIDER");
 	         btnNewButton.setForeground(new Color(255, 255, 255));
 	         btnNewButton.setBackground(new Color(0, 0, 64));
 	         btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 14));
-	         btnNewButton.setBounds(167, 218, 99, 21);
-	         jframe.getContentPane().add(btnNewButton);
+	         
+	         JButton btnModifier = new JButton("MODIFIER");
+	         btnModifier.setForeground(Color.WHITE);
+	         btnModifier.setFont(new Font("Times New Roman", Font.BOLD, 14));
+	         btnModifier.setBackground(new Color(0, 0, 64));
+	         GroupLayout groupLayout = new GroupLayout(jframe.getContentPane());
+	         groupLayout.setHorizontalGroup(
+	         	groupLayout.createParallelGroup(Alignment.LEADING)
+	         		.addGroup(groupLayout.createSequentialGroup()
+	         			.addGap(53)
+	         			.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE))
+	         		.addGroup(groupLayout.createSequentialGroup()
+	         			.addGap(38)
+	         			.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+	         			.addGap(10)
+	         			.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE))
+	         		.addGroup(groupLayout.createSequentialGroup()
+	         			.addGap(38)
+	         			.addComponent(lblChoix, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+	         			.addGap(10)
+	         			.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE))
+	         		.addGroup(groupLayout.createSequentialGroup()
+	         			.addGap(68)
+	         			.addComponent(btnModifier, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+	         			.addGap(114)
+	         			.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE))
+	         );
+	         groupLayout.setVerticalGroup(
+	         	groupLayout.createParallelGroup(Alignment.LEADING)
+	         		.addGroup(groupLayout.createSequentialGroup()
+	         			.addGap(10)
+	         			.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+	         			.addGap(26)
+	         			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+	         				.addGroup(groupLayout.createSequentialGroup()
+	         					.addGap(12)
+	         					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE))
+	         				.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+	         			.addGap(50)
+	         			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+	         				.addGroup(groupLayout.createSequentialGroup()
+	         					.addGap(12)
+	         					.addComponent(lblChoix, GroupLayout.PREFERRED_SIZE, 13, GroupLayout.PREFERRED_SIZE))
+	         				.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+	         			.addGap(35)
+	         			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+	         				.addGroup(groupLayout.createSequentialGroup()
+	         					.addGap(1)
+	         					.addComponent(btnModifier, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE))
+	         				.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)))
+	         );
+	         jframe.getContentPane().setLayout(groupLayout);
 	         
 	         // enregistrement des choix dans la base de donnée après validation
 	         
 	         btnNewButton.addActionListener(new ActionListener() {
-	             public void actionPerformed(ActionEvent e) {
+	            
+	        	 public void actionPerformed(ActionEvent e) {
 	            	 
 	            	 String username = StudentStatut.getIdStudent();
-	                 // Récupère les choix
+	                 // On récupère les choix des étudiants
 	                 List<String> choix = new ArrayList<>();
 	                 choix.add(comboBox.getSelectedItem().toString());
 	                 choix.add(comboBox_1.getSelectedItem().toString());
-
-	                 //ChoixDAO.supprimerChoixEtudiant(username);
-	                   ChoixDAO.validerChoix(username, choix);
+                      
+	              // On empêche l'étudiant de choisir plusieurs fois la même dominante 
+	                 if (choix.get(0).equals(choix.get(1))) {
+	                     JOptionPane.showMessageDialog(jframe, "Vous ne pouvez pas choisir plusieurs fois la même dominante!\n             Modifiez vos choix");
+	                     return;
+	                 }
+	             
+	               
+	              //validation des choix de l'étudiant   
+	                 ChoixDAO.validerChoix(username, choix);
 
 	                 JOptionPane.showMessageDialog(jframe, "Choix enregistrés avec succès !");
 	                 jframe.dispose();
 	             }
 	         });
 
-	         
+	         btnModifier.addActionListener(new ActionListener() {
+	        	 
+	        	 public void actionPerformed(ActionEvent e) {
+	        		 
+	        		 
+	        	 }
+	         });
 	         jframe.setVisible(true);
 	         
 	    }

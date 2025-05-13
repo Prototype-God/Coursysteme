c995418eb8ce9c77e3ace56d01033b7436fb5c36
Prototype_God <76643845+Prototype-God@users.noreply.github.com>
2025-05-13 12:05:38 +0200

    Update choix_FISE.java

--- src1/GUI/choix_FISE.java
+++ src1/GUI/choix_FISE.java
@@ -1,15 +1,11 @@
 package GUI;
 
-import dao.ConnectionDAO;
-import dao.EtudiantDAO;
-import model.Etudiants;
-import stockage.StudentStatut;
 
+import dao.EtudiantDAO;
+import stockage.StudentStatut;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.awt.Font;
 import java.awt.Color;
 import java.util.List;
@@ -38,7 +34,11 @@ public class choix_FISE {
 	         jframe1.setSize(500, 600);
 	         jframe1.getContentPane().setLayout(null);
 	         
-	         String[] tab_domin = {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"};
+	         //String[] tab_domin = {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"};
+	        
+	         List<String> dominantes = EtudiantDAO.getAllDominantes();
+	         String[] tab_domin = dominantes.toArray(new String[0]);
+	         
 	         JComboBox <String> comboBox = new JComboBox <>(tab_domin);
 	        // comboBox.setModel(new DefaultComboBoxModel(new String[] {"ESAA", "DARIA", "EDD", "IA-DES", "GET", "BDTN", "CERT", "IA-IR", "IF", "ISN", "IABD", "DLTQ", "ISEMAC", "MCTSE"}));
 	          comboBox.setBounds(103, 61, 258, 36);
@@ -104,14 +104,22 @@ public class choix_FISE {
 	         btnNewButton.setForeground(new Color(255, 255, 255));
 	         btnNewButton.setBackground(new Color(64, 0, 64));
 	         btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 14));
-	         btnNewButton.setBounds(173, 494, 116, 21);
+	         btnNewButton.setBounds(288, 490, 116, 21);
 	         jframe1.getContentPane().add(btnNewButton);
+	         
+	         JButton btnModifier = new JButton("MODIFIER");
+	         btnModifier.setForeground(Color.WHITE);
+	         btnModifier.setFont(new Font("Times New Roman", Font.BOLD, 14));
+	         btnModifier.setBackground(new Color(64, 0, 64));
+	         btnModifier.setBounds(59, 491, 116, 21);
+	         jframe1.getContentPane().add(btnModifier);
 	         
 	         btnNewButton.addActionListener(new ActionListener() {
 	             public void actionPerformed(ActionEvent e) {
 	            	 
 	         String username = StudentStatut.getIdStudent();
-	         // validation des choix
+	         
+	         // enregistrement ou récupération des choix
 	         List<String> choixDominantes = new ArrayList<>();
 	            choixDominantes.add(comboBox.getSelectedItem().toString());
 	            choixDominantes.add(comboBox_1.getSelectedItem().toString());
@@ -119,6 +127,27 @@ public class choix_FISE {
 	            choixDominantes.add(comboBox_3.getSelectedItem().toString());
 	            choixDominantes.add(comboBox_4.getSelectedItem().toString());
 
+	        
+	            // On empêche l'étudiant d'effectuer plusieurs fois le même choix
+	            
+	            boolean doublon=false;
+	            for (int i=0; i < choixDominantes.size(); i++) {
+	            	for(int j=i+1; j  < choixDominantes.size(); j++)
+	            	{
+	            	if (choixDominantes.get(i).equals(choixDominantes.get(j))) {
+	            		  doublon = true;
+	            		  break;
+	            	     }
+	                }
+	            if(doublon) break;	
+	            }
+	            
+	            if(doublon) {
+	            JOptionPane.showMessageDialog(jframe1, "Vous ne pouvez pas choisir plusieurs fois la même dominante!\n             Modifiez vos choix");
+                return;
+	            }
+	           
+	            // validation des choix
 	            ChoixDAO.validerChoix(username, choixDominantes);
 	            JOptionPane.showMessageDialog(jframe1, "Choix enregistrés avec succès !");
                 jframe1.dispose();
@@ -131,3 +160,4 @@ public class choix_FISE {
 	         
 	    }
 }
+
